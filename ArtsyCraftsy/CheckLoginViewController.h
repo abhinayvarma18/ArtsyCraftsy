07f903d06e573f06de85e32f2ab4b589bade4637
Abhinay.Varma <Abhinay.Varma@metricstream.com>
2016-11-02 11:09:05 +0530

    first commit

---
+++ ArtsyCraftsy/CheckLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  CheckLoginViewController.h
+//  ArtsyCraftsy
+//
+//  Created by Abhinay Varma on 01/11/16.
+//  Copyright © 2016 Abhinay Varma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckLoginViewController : UIViewController
+
+@end
---
+++ ArtsyCraftsy/CheckLoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  CheckLoginViewController.m
+//  ArtsyCraftsy
+//
+//  Created by Abhinay Varma on 01/11/16.
+//  Copyright © 2016 Abhinay Varma. All rights reserved.
+//
+
+#import "CheckLoginViewController.h"
+
+@interface CheckLoginViewController ()
+
+@end
+
+@implementation CheckLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
