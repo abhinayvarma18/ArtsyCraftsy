2222c52a3e5439d85d0657bea58e97ba9c526ee9
ayunawani <ayu.nawani@gmail.com>
2016-11-05 14:42:32 +0530

    new theme added for ui

--- ArtsyCraftsy/FeedsViewController.m
+++ ArtsyCraftsy/FeedsViewController.m
@@ -8,15 +8,21 @@
 
 #import "FeedsViewController.h"
 #import "SWRevealViewController.h"
-
+@import Firebase;
 @interface FeedsViewController ()
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
-
+@property (strong, nonatomic) FIRDatabaseReference *ref;
+@property (strong, nonatomic) IBOutlet UICollectionView *collectionView;
 @end
 
 @implementation FeedsViewController
+{
+    NSArray *recipeImages;
+    NSMutableArray *stories;
+}
 
 - (void)viewDidLoad {
+    recipeImages = [NSArray arrayWithObjects:@"mybgnew2.jpg", @"mybgnew4.jpg", @"mybgnew2.jpg",@"mybgnew4.jpg",@"mybgnew7.jpeg",@"mybgnew4.jpg", @"mybgnew2.jpg", @"mybgnew7.jpeg",nil];
     [super viewDidLoad];
     SWRevealViewController *revealViewController = self.revealViewController;
     if ( revealViewController ){
@@ -24,12 +30,55 @@
         [self.sidebarButton setAction: @selector( revealToggle: )];
         [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
     }
+    
+    self.ref = [[FIRDatabase database] reference];
+    [[self.ref child:@"stories"] observeSingleEventOfType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
+        // Get user value
+        stories = [NSMutableArray array];
+        NSDictionary *postDict = snapshot.value;
+        //stories = snapshot.value;
+        int i = 0;
+        for (id obj in postDict) {
+            [stories  insertObject:[postDict objectForKey:obj] atIndex:i ];
+            i++;
+        }
+        NSArray *ss = stories[0];
+        [self.collectionView reloadData];
+        
+        // ...
+    } withCancelBlock:^(NSError * _Nonnull error) {
+        NSLog(@"%@", error.localizedDescription);
+    }];
     // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return recipeImages.count;
+}
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *identifier = @"Cell";
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
+    
+    UIImageView *recipeImageView = (UIImageView *)[cell viewWithTag:100];
+    UILabel *title = (UILabel *)[cell viewWithTag:50];
+    NSInteger index = indexPath.row;
+    if(stories != nil) {
+        if(index < stories.count) {
+        NSDictionary *story = stories[indexPath.row];
+        title.text = [story objectForKey:@"text"];
+        }
+    }
+    recipeImageView.image = [UIImage imageNamed:[recipeImages objectAtIndex:indexPath.row]];
+    
+    return cell;
 }
 
 /*
