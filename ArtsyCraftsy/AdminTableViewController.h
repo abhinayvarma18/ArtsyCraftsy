a9162163cdfb97f170c0f8e65a962cfdead34725
abhinayvarma18 <abhinaycpian@gmail.com>
2016-11-07 19:00:30 +0530

    admin tableview controller

---
+++ ArtsyCraftsy/AdminTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AdminTableViewController.h
+//  ArtsyCraftsy
+//
+//  Created by Abhinay Varma on 07/11/16.
+//  Copyright © 2016 Abhinay Varma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdminTableViewController : UITableViewController
+
+@end
